9469ce141621bc5e6a11f286cd277d2cdc4e68d0
Mivia Li <32752716+Mivialll@users.noreply.github.com>
2018-06-10 14:10:14 -0400

    Update server.js

--- Chatbox/server.js
+++ Chatbox/server.js
@@ -8,7 +8,8 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server running...')
 
-app.get('/', function(req, res){
+app.get('/', function(req, res)
+	{
 	res.sendFile(__dirname + '/index.html');
 });
 
